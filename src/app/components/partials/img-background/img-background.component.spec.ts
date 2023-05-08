efa73944c72f6c4058d6361da0ade64d880390ab
Gabriela Duran <gabrieladuran013@gmail.com>
2023-05-08 17:09:56 -0300

    Final

---
+++ src/app/components/partials/img-background/img-background.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ImgBackgroundComponent } from './img-background.component';
+
+describe('ImgBackgroundComponent', () => {
+  let component: ImgBackgroundComponent;
+  let fixture: ComponentFixture<ImgBackgroundComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ImgBackgroundComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ImgBackgroundComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/partials/img-background/img-background.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-img-background',
+  templateUrl: './img-background.component.html',
+  styleUrls: ['./img-background.component.css']
+})
+export class ImgBackgroundComponent {
+
+}
